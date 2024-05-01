b75505af075dde0f21a66f497ed17de5e3f2f0ad
nitinsreevstv <76206822+nitinsreevstv@users.noreply.github.com>
2024-05-01 19:40:47 +0530

    added links behind social media and photos in a folder now

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// script.js
-// 
-
 document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('#element', {
         strings: ['Software Quality Test', '&amp; Automate Test Cases.'],
@@ -12,16 +9,23 @@ document.addEventListener('DOMContentLoaded', function () {
         var resumeUrl = 'Priyanshu_resume.docx'; // Replace with your actual resume file URL
         var link = document.createElement('a');
         link.href = resumeUrl;
-        link.download = 'Priyanshu_resume.docx'; 
+        link.download = 'Priyanshu_resume.docx';
         link.click();
     };
 
     // Add event listener for clicking on the "Services" link
     document.getElementById('services-link').addEventListener('click', function (event) {
         event.preventDefault(); // Prevent default link behavior
-        
+
         // Scroll to the "Work Experience" section
         document.getElementById('work-experience-section').scrollIntoView({ behavior: 'smooth' });
     });
-});
 
+    // Add event listener for clicking on the "Skills" link
+    document.querySelector('nav ul li a[href="#skillsSection"]').addEventListener('click', function (event) {
+        event.preventDefault(); // Prevent default link behavior
+
+        // Scroll to the "Skills" section
+        document.getElementById('skillsSection').scrollIntoView({ behavior: 'smooth' });
+    });
+});
